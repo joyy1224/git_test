7cb34a5a649cd1633040f3e59ba0e9e03313cc1b
Ityue <921826654@qq.com>
2022-08-27 18:27:17 +0800

    second

--- src/main/java/com/ityue/controller/gitcontroller.java
+++ src/main/java/com/ityue/controller/gitcontroller.java
@@ -3,5 +3,6 @@ package com.ityue.controller;
 public class gitcontroller {
     public void helloGit(){
         System.out.println("hello git!");
+        System.out.println("hello gitHub!");
     }
 }
