58de1f6b018d26f85f14273208588a09111f9ee9
Ityue <921826654@qq.com>
2022-08-27 20:33:55 +0800

    hot-fix first

--- src/main/java/com/ityue/controller/gitcontroller.java
+++ src/main/java/com/ityue/controller/gitcontroller.java
@@ -5,5 +5,6 @@ public class gitcontroller {
         System.out.println("hello git!");
         System.out.println("hello gitHub!");
         System.out.println("hello gitHub1!");
+        System.out.println("hello gitHub2!");
     }
 }
