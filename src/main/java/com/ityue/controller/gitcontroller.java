1521d76010b3b4394dfd3522792316edd6366d57
Ityue <921826654@qq.com>
2022-08-27 18:28:48 +0800

    thrid

--- src/main/java/com/ityue/controller/gitcontroller.java
+++ src/main/java/com/ityue/controller/gitcontroller.java
@@ -4,5 +4,6 @@ public class gitcontroller {
     public void helloGit(){
         System.out.println("hello git!");
         System.out.println("hello gitHub!");
+        System.out.println("hello gitHub1!");
     }
 }
